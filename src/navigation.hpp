9bdd358583557f7a017f42bbd49aa7456ed5bc17
Olle Mattsson <olle@rymdis.com>
2024-01-12 12:20:58 +0100

    Add navigation options

--- src/navigation.hpp
+++ src/navigation.hpp
@@ -10,19 +10,32 @@ class Navigation
 public:
   void display()
   {
-    for (size_t i = 0; i < options.size(); ++i) {
-      std::cout << i+1 << ". " << options[i] << '\n';
+    for (size_t i = 0; i < main_options.size(); ++i) {
+      std::cout << i+1 << ". " << main_options[i] << '\n';
     }
   }
 
 private:
-  std::vector<std::string> options
+  std::vector<std::string> main_options
     {
       "Account and balance",
       "Transfer",
       "Exchange",
       "Logout",
     };
+
+  std::vector<std::string> account_options {
+    "Back"
+  };
+
+  std::vector<std::string> transfer_options {
+    "Back"
+  };
+
+  std::vector<std::string> exchange_options {
+    "Back"
+  };
+
 };
 
 #endif /* NAVIGATION_H */
